276c50b803b1191023d7b945008bf6768c0055b0
serj053 <serj053@yandex.ru>
2024-06-01 16:13:57 +0300

    Выполнил правки.

--- src/lesson1/Homework1.java
+++ src/lesson1/Homework1.java
@@ -5,6 +5,6 @@ public class Homework1 {
         task1();
     }
     private static void task1(){
-        System.out.println("Решение первой задачи");
+        System.out.println("Решение первой задачи !");
     }
 }
