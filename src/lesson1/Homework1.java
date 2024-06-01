0a1c7075faf2e16e8095a8c31f8fb3bcb4b61c7d
serj053 <serj053@yandex.ru>
2024-06-01 14:07:43 +0300

    Выполнил первое ДЗ

---
+++ src/lesson1/Homework1.java
@@ -0,0 +1,10 @@
+package lesson1;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        task1();
+    }
+    private static void task1(){
+        System.out.println("Решение первой задачи");
+    }
+}
